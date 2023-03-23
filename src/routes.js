5e755e4c74e3a89aaece7669b86bf25ffd39fdfd
Paulo Souza <jr.paulo87@gmail.com>
2023-03-23 06:33:33 -0300

    feat: add basic route handler

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+
+export const routes = [
+   {
+      method: 'GET',
+      path: '/tasks',
+      handler: (req, res) => {
+         console.log('tasks')
+         return res.end()
+      }
+   },
+
+   {
+      method: '',
+      path: '',
+      handler: (req, res) => {
+         console.log('erro')
+         return res.end()
+      }
+   },
+
+   {
+      method: '',
+      path: '',
+      handler: (req, res) => {
+         return res.end()
+      }
+   },
+]
